97980e4782ceeba6f743e6a940a38fbdb0138ed5
sfjbs <kirankosanam1@gmail.com>
2023-09-04 05:42:24 +0000

    Create 2 spring-boot microservices web and non-web

---
+++ day02/kiran-web-microservice/src/main/java/com/kiran/app/FirstWebMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebMicroserviceApplication.class, args);
+	}
+
+}
---
+++ day02/kiran-web-microservice/src/test/java/com/kiran/app/FirstWebMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
